03a81c579c67bb984cd6e8194351d7bdd69df95b
moetaz <3ezo.yasser@gmail.com>
2024-11-11 20:00:31 -0500

    first commit frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'seat_availibilty_frontend';
+  public seating : any;
+  private  httpClient = inject(HttpClient)
+  ngOnInit() {
+    this.seating  = this.httpClient.get<any>("http://localhost:3000/seats").subscribe(seating=> console.log(seating))
+
+  }
+
+  ngOnDestroy() {
+    
+  }
+
+  get totalSeats(): number {
+    return this.seating.length;
+  }
+
+  // get availableSeats(): number {
+  //   return this.seating.filter(seat => seat.available).length;
+  // }
 }
